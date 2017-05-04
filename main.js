1d8829cb1f25c59d4114a1382be128e7d2563f32
Kenta Kase <kesin1202000@gmail.com>
2017-05-04 15:05:36 +0900

    add global shortcut

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const electron = require('electron')
-const clipboard = require('electron').clipboard
+const clipboard = electron.clipboard
+const globalShortcut = electron.globalShortcut
 const robot = require('robotjs')
 
 // Module to control application life.
@@ -38,15 +39,18 @@ function createWindow () {
 
   setInterval(() => {
     console.log("test")
-    console.log(clipboard.readText())
-    robot.keyTap("enter")
+    // console.log(clipboard.readText())
+    // robot.keyTap("enter")
   }, 1000)
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  registerGlobalShortcut()
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -67,3 +71,14 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+app.on('will-quit', () => {
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll()
+})
+
+function registerGlobalShortcut () {
+  globalShortcut.register('CommandOrControl+Shift+V', () => {
+    console.log('CommandOrControl+Shift+V is pressed')
+  })
+}
