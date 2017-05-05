76a8add89e9eb4edd2d7900fca170ce022d8c0a4
Kenta Kase <kesin1202000@gmail.com>
2017-05-05 13:35:49 +0900

    Add vuejs and simple render without webpack

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -51,26 +51,6 @@ app.on('ready', () => {
   })
   registerGlobalShortcut()
 })
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 app.on('will-quit', () => {
   // Unregister all shortcuts.
--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+// vueではなくてvue/dist/vue.jsをrequireするとruntime + compilerとなる
+// https://stackoverflow.com/questions/39488660/vue-js-2-0-not-rendering-anything
+const Vue = require('vue/dist/vue.js')
+console.log('require renderer')
+
+const app = Vue.component('app', {
+  template: `
+    <div>app {{ message }}</div>
+  `,
+  data: () => {
+      return {
+        message: 'todo',
+    }
+  }
+})
+new Vue({
+  el: '#app',
+  render: h => h(app),
+})
